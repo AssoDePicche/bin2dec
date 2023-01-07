efab05c377527afcbe236867b79300722b5e1032
AssoDePicche <samuelprado730@gmail.com>
2023-01-07 02:04:15 -0300

    Refactor: Remove AttributeToggle.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-import AttributeToggle from "./modules/AttributeToggle.js";
 import B2D from "./modules/B2D.js";
 
-const input = document.getElementById("binary");
+const input = document.getElementById("input");
 
 const output = document.getElementById("output");
-
-const b2d = document.getElementById("b2d");
 
 const clear = document.getElementById("clear");
 
@@ -14,31 +11,25 @@ clear.addEventListener("click", () => {
 
   output.value = "";
 
-  AttributeToggle.disable(b2d);
-
-  AttributeToggle.disable(clear);
+  clear.setAttribute("disabled", "true");
 });
 
 input.addEventListener("input", () => {
   let value = input.value;
 
-  if (value.length > 0) {
-    b2d.removeAttribute("disabled");
+  if (value.length === 0) {
+    input.value = "";
+    output.value = "";
+    clear.setAttribute("disabled", "true");
+  }
 
+  if (value.length > 0) {
     clear.removeAttribute("disabled");
   }
 
-  if (!value.match(/^[01]+$/)) {
-    alert("Type neither 0 or 1");
-
-    input.value = "";
-
-    AttributeToggle.disable(b2d);
-
-    AttributeToggle.disable(clear);
+  if (!value.match(/^[01]+$/) && value.length > 0) {
+    input.value = input.value.substring(0, value.length - 1);
   }
-});
 
-b2d.addEventListener("click", () => {
   output.value = B2D.convert(input.value);
 });
